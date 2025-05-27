70df3b3ccb61dc4cf9cf93e09635d145bbd02d99
FadhilM <mudzakifadhil@gmail.com>
2025-05-27 15:40:47 +0700

    classs dokter

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-class dokter;
+class dokter; 
 
 
 class pasien{  //asosiasi
@@ -16,4 +16,21 @@ class pasien{  //asosiasi
             cout << "Pasien \"" << nama
             << "\" tidak ada\n";
         }
-}
+        void tambahDokter(dokter);
+        void cetakDokter();
+};
+
+class dokter {
+    public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+
+        dokter(string pNama): nama(pNama) {
+            cout << "dokter \"" << nama << "\" ada\n";
+        }
+        ~dokter() {
+            cout << "Dokter \"" << nama
+            << "\" tidak ada\n";
+        }
+
+};
