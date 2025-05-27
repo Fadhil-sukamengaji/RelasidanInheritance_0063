230543fbccc4819356f57ff97c6c0299dc3212eb
FadhilM <mudzakifadhil@gmail.com>
2025-05-27 15:20:32 +0700

    int main

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -4,3 +4,16 @@ using namespace std;
 
 #include "anak.H"
 #include "ibu.H"
+
+int main() {
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("fadhil");
+    anak* varAnak2 = new anak("sultan");
+    anak* varAnak3 = new anak("faiz");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+};
